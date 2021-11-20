7f5b3dbe573ca19155c2e6e416a4a3d45c2af97a
Rodewitsch <rodewitsch@inbox.ru>
2021-11-20 20:54:12 +0300

    fix: interval fix and refactoring
    fix async function in interval, change executeRule function logic: eject reloadPM2Process outside
    
    #11

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { log } = require('./lib/log');
-const { checkPM2ModuleExists } = require('./lib/pm2');
+const { checkPM2ModuleExists, reloadPM2Process } = require('./lib/pm2');
 const { delay, getMiliseconds } = require('./lib/time');
 const { processingArguments } = require('./lib/args');
 const {
@@ -29,20 +29,28 @@ async function main() {
 
     // TODO: rules syntax validator
 
-    for (const rule of rules) {
+    rules.forEach(async (rule) => {
       do {
-        const skipped = await executeRule(
-          rule,
-          rule.options.skip && rule.options.skipped !== rule.options.skip
-        );
+        const ruleStatus = await executeRule(rule);
+        if (!rule.options.skip && ruleStatus.matched) {
+          await reloadPM2Process(ruleStatus.name);
+        }
         if (rule.options.skip) {
           rule.options.skipped = rule.options.skipped + 1 || 1;
-          if (!skipped) rule.options.skipped = 1;
+          if (
+            ruleStatus.matched &&
+            rule.options.skip === rule.options.skipped
+          ) {
+            await reloadPM2Process(ruleStatus.name);
+            rule.options.skipped = 1;
+          }
+          if (!ruleStatus.matched) rule.options.skipped = 1;
         }
-        if (rule.options.interval)
+        if (rule.options.interval) {
           await delay(getMiliseconds(rule.options.interval));
+        }
       } while (rule.options.interval);
-    }
+    });
   } catch (err) {
     log(err.message, 'error');
     log('------------------pm2-supervisor errored------------------', 'error');
